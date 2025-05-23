16cf36cb1499fee994b4bdc2922d83dd2d195566
cord70 <emd.kornienko@gmail.com>
2025-05-23 20:57:39 +0300

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,26 @@
+  var menu='\
+<h4> сайт cord70.github.io/photo</h4>\
+  <p><a href="photo/index.html"> Фото о природе</a></p>\
+  <p><a href="workshop/index.html"> Самоделки</a></p>\
+  <p><a href="photo/red-sea/index.html"> На Красном море</a></p>\
+  <p><a href="photo/red-sea-fish/index.html"> Рыбы Красного моря</a></p>\
+  <p><a href="photo/underwater/index.html"> Плаваем с маской</a></p>\
+  <p><a href="photo/tank/index.html"> Аквариум</a></p>\
+  <p><a href="photo/crete/index.html"> Отпуск на Крите</a></p>\
+  <p><a href="txt/poem.html"> Стихи</a></p>\
+<p> &nbsp;</p>\
+  <p><a href="search.html"> Поиск по сайту</a></p>\
+  <p><a href="en.html"> English page</a></p>';
+
+  var menuen='\
+<h4> the site cord70.github.io/photo</h4>\
+  <p><a href="photo/en.html"> Interesting nature</a></p>\
+  <p><a href="workshop/en.html"> Homemade</a></p>\
+  <p><a href="photo/red-sea/en.html"> Red Sea</a></p>\
+  <p><a href="photo/red-sea-fish/en.html"> Red Sea fish</a></p>\
+  <p><a href="photo/underwater/en.html"> Swimming with mask</a></p>\
+  <p><a href="photo/tank/en.html"> My aquarium</a></p>\
+  <p><a href="photo/crete/en.html"> Crete</a></p>\
+<p> &nbsp;</p>\
+  <p><a href="search-en.html"> Search at this site</a></p>\
+  <p><a href="index.html"> Русская страница</a></p>';
