82e61778fcc3bea7992bfef54fc6de70ca35c705
cord70 <emd.kornienko@gmail.com>
2025-05-23 23:07:18 +0300

    Add files via upload

---
+++ photo/red-sea-fish/puffer/navPuffer.js
@@ -0,0 +1,15 @@
+var menu='\
+<h4><a href="../index.html"> Рыбы Красного моря</a></h4>\
+<h4><a href="index.html"> Масковый пуффер</a></h4>\
+  <p><a href="angry.html"> Приготовился укусить</a></p>\
+  <p><a href="back.html"> Виляет хвостом</a></p>\
+  <p><a href="fire-coral.html"> У огненного коралла</a></p>\
+  <p><a href="four-teeth.html"> У рогатого коралла</a></p>';
+
+var menuen='\
+<h4><a href="../en.html"> Red Sea Fish</a></h4>\
+<h4><a href="en.html"> Masked puffer</a></h4>\
+  <p><a href="angry-en.html"> Angry</a></p>\
+  <p><a href="back-en.html"> Waving tail</a></p>\
+  <p><a href="fire-coral-en.html"> Near fire coral</a></p>\
+  <p><a href="four-teeth-en.html"> Near horny coral</a></p>';
---
+++ photo/red-sea-fish/scat/navScat.js
@@ -0,0 +1,17 @@
+var menu='\
+<h4><a href="../index.html"> Рыбы Красного моря</a></h4>\
+<h4><a href="index.html"> Скат Тэниура лимма</a></h4>\
+  <p><a href="deep.html"> Скат в глубине</a></p>\
+  <p><a href="over-coral.html"> Скат над кораллом</a></p>\
+  <p><a href="blue_spot.html"> Скат без хвоста</a></p>\
+  <p><a href="stingray-blue-spotted.html"> Шипы</a></p>\
+  <p><a href="under-coral.html"> Под кораллом</a></p>';
+
+var menuen='\
+<h4><a href="../en.html"> Red Sea Fish</a></h4>\
+<h4><a href="en.html"> Blue-spotted stingray</a></h4>\
+  <p><a href="over-coral-en.html"> Stingray over coral</a></p>\
+  <p><a href="stingray-blue-spotted-en.html"> Blue spots</a></p>\
+  <p><a href="deep-en.html"> A ray in deep</a></p>\
+  <p><a href="blue_spot-en.html"> Shorten tail</a></p>\
+  <p><a href="under-coral-en.html"> Under coral</a></p>';
