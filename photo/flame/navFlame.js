adb2c9c8ec97e57dedd6e8bbf9d30d74008a5aa5
cord70 <emd.kornienko@gmail.com>
2025-05-23 22:06:25 +0300

    Add files via upload

---
+++ photo/flame/navFlame.js
@@ -0,0 +1,15 @@
+var menu='\
+<h4><a href="index.html"> Огонь</a></h4>\
+  <p><a href="candle.html"> Пламя свечи</a></p>\
+  <p><a href="hot-flame.html"> Ровное горение спички</a></p>\
+  <p><a href="match-sparks.html"> Спичка разгорается</a></p>\
+<h4><a href="../water/index.html"> Вода</a></h4>\
+<h4><a href="../view/index.html"> Пейзаж</a></h4>';
+
+var menuen='\
+<h4><a href="en.html"> Flame</a></h4>\
+  <p><a href="match-sparks-en.html"> Match starts burning</a></p>\
+  <p><a href="candle-en.html"> Wax candle</a></p>\
+  <p><a href="hot-flame-en.html"> Calm burning</a></p>\
+<h4><a href="../water/en.html"> Water</a></h4>\
+<h4><a href="../view/en.html"> View</a></h4>';
