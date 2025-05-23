f20aa45e8b9448ac53da654282f292c980a513bf
cord70 <emd.kornienko@gmail.com>
2025-05-23 21:44:54 +0300

    Add files via upload

---
+++ workshop/navWorkshop.js
@@ -0,0 +1,30 @@
+var menu='\
+<h4> Самоделки</h4>\
+  <p><a href="rubik.html"> Сборка кубика Рубика</a></p>\
+  <p><a href="HP-C200.html"> Макро мыльницей</a></p>\
+  <p><a href="ring-light.html"> Кольцевая лампа</a></p>\
+  <p><a href="noiseless.html"> Шумоглушение компьютера</a></p>\
+  <p><a href="site.html"> Мой сайт</a></p>\
+<h4><a href="da4a/da4a.html"> Дача и пруд</a></h4>\
+<h4> Аквариум</h4>\
+  <p><a href="fish_home.html"> Грот в аквариуме</a></p>\
+  <p><a href="co2/generator-co2.html"> Генератор CO2</a></p>\
+<h4> Аккумулятор</h4>\
+  <p><a href="accu-capacity.html"> Измерение ёмкости</a></p>\
+  <p><a href="accu-meter-circuitry.html"> Пример платы</a></p>\
+  <p><a href="accu-meter2.html"> Схема для АА</a></p>\
+  <p><a href="internal-resistance.html"> Внутреннее сопротивление</a></p>\
+<h4><a href="pro/index.html"> PC и программы</a></h4>\
+<h4> Новости техники</h4>\
+  <p><a href="blink-stereo.html"> Блинк-стерео фото</a></p>\
+  <p><a href="costa-concordia.html"> Коста Конкордия</a></p>\
+  <p><a href="hyperloop.html"> Hyperloop</a></p>';
+
+var menuen='\
+<h4> Homemade</h4>\
+  <p><a href="fish_home-en.html"> A cave for fish</a></p>\
+  <p><a href="accu-capacity-en.html"> Accumulator capacity meter</a></p>\
+  <p><a href="accu-meter-circuitry-en.html"> • Wire-arrangement</a></p>\
+  <p><a href="accu-meter2-en.html"> • Example for AA</a></p>\
+  <p><a href="noiseless-en.html"> Noiseless computer</a></p>\
+  <p><a href="HP-C200-en.html"> Macro with a p/s camera</a></p>';
