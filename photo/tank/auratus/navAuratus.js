f5d44c5d35728124b824d3cfad657fbc4a02547c
cord70 <emd.kornienko@gmail.com>
2025-05-23 23:01:55 +0300

    Add files via upload

---
+++ photo/tank/auratus/navAuratus.js
@@ -0,0 +1,15 @@
+var menu='\
+<h4><a href="../index.html"> Аквариум</a></h4>\
+<h4><a href="index.html"> Ауратус</a></h4>\
+  <p><a href="male.html"> Ауратус самец</a></p>\
+  <p><a href="female.html"> Молодой Ауратус</a></p>\
+  <p><a href="female-mature.html"> Ауратус, самка</a></p>\
+  <p><a href="auratus.html"> Умный ауратус</a></p>';
+
+var menuen='\
+<h4><a href="../en.html"> Aquarium</a></h4>\
+<h4><a href="en.html"> Auratus</a></h4>\
+  <p><a href="auratus-en.html"> Little</a></p>\
+  <p><a href="female-en.html"> Young</a></p>\
+  <p><a href="female-mature-en.html"> Female</a></p>\
+  <p><a href="male-en.html"> Male</a></p>';
---
+++ photo/tank/macrognatus/navMacrognatus.js
@@ -0,0 +1,17 @@
+var menu='\
+<h4><a href="../index.html"> Аквариум</a></h4>\
+<h4><a href="index.html"> Макрогнатус</a></h4>\
+  <p><a href="hole.html"> В норке</a></p>\
+  <p><a href="out.html"> Наблюдает</a></p>\
+  <p><a href="tail.html"> Спрятал голову в песке</a></p>\
+  <p><a href="two-tails.html"> Два хвоста</a></p>\
+  <p><a href="head.html"> Выглядывает из грунта</a></p>';
+
+var menuen='\
+<h4><a href="../en.html"> Aquarium</a></h4>\
+<h4><a href="en.html"> Macrognatus</a></h4>\
+  <p><a href="hole-en.html"> In a hole</a></p>\
+  <p><a href="out-en.html"> Watches around</a></p>\
+  <p><a href="tail-en.html"> Tail with eyes</a></p>\
+  <p><a href="two-tails-en.html"> Two friends</a></p>\
+  <p><a href="head-en.html"> Head</a></p>';
