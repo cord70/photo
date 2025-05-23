ea554161edf116ffc4bfb3a1d0f639b9b28d8365
cord70 <emd.kornienko@gmail.com>
2025-05-23 21:48:27 +0300

    Add files via upload

---
+++ workshop/da4a/navDa4a.js
@@ -0,0 +1,9 @@
+var menu='\
+<h4> Дача</h4>\
+  <p><a href="da4a.html"> Умная дача</a></p>\
+  <p><a href="adc.html"> Считывание счётчика</a></p>\
+  <p><a href="firewood-shed.html"> Дровница</a></p>\
+<h4> Пруд</h4>\
+  <p><a href="../pond/ornamental-pond.html"> Декоративный пруд</a></p>\
+  <p><a href="../pond/bush.html"> • Удаление куста</a></p>\
+  <p><a href="../pond/water-for-pond.html"> • Вода для пруда</a></p>';
