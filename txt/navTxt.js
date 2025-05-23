5acfe760ffc09298f2385cc9b7594fc2bb151ccf
cord70 <emd.kornienko@gmail.com>
2025-05-23 21:43:18 +0300

    Add files via upload

---
+++ txt/navTxt.js
@@ -0,0 +1,9 @@
+var menu=
+'<h4> Стихи</h4>						\
+  <p><a href="omar.html">	О.Хайям об учёности</a></p>	\
+  <p><a href="omar-retouch.html"> О.Хайям, любимые</a></p>	\
+  <p><a href="omar-life.html">	О.Хайям о жизни</a></p>		\
+  <p><a href="rubayyat.html">	Мои рубаи</a></p>		\
+  <p> &nbsp;</p>						\
+  <p><a href="love-me.html">	Любишь ты меня</a></p>		\
+  <p><a href="sonet-0.html">	Сонет шесть строк</a></p>	';
