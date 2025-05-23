459d436855ca8928fce569662c35dbae477d96ce
cord70 <emd.kornienko@gmail.com>
2025-05-23 22:01:39 +0300

    Add files via upload

---
+++ photo/astro/navAstro.js
@@ -0,0 +1,19 @@
+var menu='\
+<h4><a href="index.html"> Простое астрофото</a></h4>\
+  <p><a href="lomonosov.html"> Открытие атмосферы Венеры</a></p>\
+  <p><a href="mercury-sun.html"> Прохождение Меркурия</a></p>\
+  <p><a href="small-sun-eclipse.html"> Затмение Солнца</a></p>\
+  <p><a href="moon.html"> Фазы лунного затмения</a></p>\
+<h4> Астро-новости</h4>\
+  <p><a href="lomonosov-text.html"> Ломоносов, текст 1761</a></p>\
+  <p><a href="sol369.html"> Затмение на Марсе</a></p>\
+  <p><a href="nova-2013.html"> Новая Delphini 2013</a></p>\
+  <p><a href="grasshoper-test.html"> SpaceX Grasshoper</a></p>\
+  <p><a href="sol351.html"> Затмение Деймоса</a></p>\
+  <p><a href="titan-sea.html"> Море на Титане</a></p>';
+
+var menuen='\
+<h4><a href="en.html"> Simple astrophoto</a></h4>\
+  <p><a href="moon-en.html"> Moon eclipse</a></p>\
+  <p><a href="mercury-sun-en.html"> Mercury transit</a></p>\
+  <p><a href="small-sun-eclipse-en.html"> Partial Sun eclipse</a></p>';
