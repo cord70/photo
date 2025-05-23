150a6abf20749f7c9a846eed154f1a07c7a1029c
cord70 <emd.kornienko@gmail.com>
2025-05-23 23:13:22 +0300

    Add files via upload

---
+++ photo/underwater/coral/navCoral.js
@@ -0,0 +1,25 @@
+var menu='\
+<h4><a href="../index.html"> Плаваем с маской</a></h4>\
+<h4><a href="index.html"> Кораллы</a></h4>\
+  <p><a href="fire-macro.html"> Жгучие иголочки</a></p>\
+  <p><a href="acropora.html"> Симбиоз кораллов</a></p>\
+  <p><a href="brain.html"> Мозговик</a></p>\
+  <p><a href="soft.html"> Нежный коралл</a></p>\
+  <p><a href="brain-labyrinth.html"> Лабиринтовый узор</a></p>\
+  <p><a href="fire.html"> Огненный коралл</a></p>\
+  <p><a href="gardineroseris.html"> Ребристые ячейки</a></p>\
+  <p><a href="sarcophyton.html"> Мягкий коралл</a></p>\
+  <p><a href="staghorn.html"> Рогатый коралл</a></p>';
+
+var menuen='\
+<h4><a href="../en.html"> Swimming with mask</a></h4>\
+<h4><a href="en.html"> Corals</a></h4>\
+  <p><a href="fire-macro-en.html"> Stinging needles</a></p>\
+  <p><a href="brain-en.html"> Brain-like coral</a></p>\
+  <p><a href="brain-labyrinth-en.html"> Labyrinth pattern</a></p>\
+  <p><a href="fire-en.html"> Fire coral</a></p>\
+  <p><a href="gardineroseris-en.html"> Pattern of holes</a></p>\
+  <p><a href="acropora-en.html"> Two corals</a></p>\
+  <p><a href="sarcophyton-en.html"> Soft coral</a></p>\
+  <p><a href="soft-en.html"> Tender coral</a></p>\
+  <p><a href="staghorn-en.html"> Hard horny coral</a></p>';
