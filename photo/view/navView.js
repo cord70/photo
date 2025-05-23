e3b6d473bc28a23d32d9805c3825fa2aac32d8b8
cord70 <emd.kornienko@gmail.com>
2025-05-23 22:11:16 +0300

    Add files via upload

---
+++ photo/view/navView.js
@@ -0,0 +1,7 @@
+var menu=
+'<h4><a href="index.html"> Пейзаж</a></h4>\
+  <p><a href="forest.html"> Край леса</a></p>\
+<h4> Натюрморт</h4>\
+  <p><a href="steel-spring.html"> Пружина</a></p>\
+<h4><a href="../water/index.html"> Вода</a></h4>\
+<h4><a href="../flame/index.html"> Огонь</a></h4>';
---
+++ photo/water/navWater.js
@@ -0,0 +1,23 @@
+var menu='\
+<h4><a href="index.html"> Вода</a></h4>\
+  <p><a href="fir-water.html"> Капля в хвое</a></p>\
+  <p><a href="rainy.html"> Дождь в лесу</a></p>\
+  <p><a href="shower.html"> Душ</a></p>\
+  <p><a href="water-form.html"> Форма воды</a></p>\
+  <p><a href="rainy-bog.html"> Дождь в лесу</a></p>\
+  <p><a href="sea.html"> Виды моря</a></p>\
+  <p><a href="falling-drop.html"> Падающая капля</a></p>\
+<h4><a href="../flame/index.html"> Огонь</a></h4>\
+<h4><a href="../view/index.html"> Пейзаж</a></h4>';
+
+var menuen='\
+<h4><a href="en.html"> Water</a></h4>\
+  <p><a href="falling-drop-en.html"> Falling drop</a></p>\
+  <p><a href="water-form-en.html"> Form of water</a></p>\
+  <p><a href="shower-en.html"> Macro of shower</a></p>\
+  <p><a href="rainy-bog-en.html"> Rain on a forest bog</a></p>\
+  <p><a href="sea-en.html"> Sea views</a></p>\
+  <p><a href="fir-water-en.html"> Water in fir</a></p>\
+  <p><a href="rainy-en.html"> Rain in forest</a></p>\
+<h4><a href="../flame/en.html"> Flame</a></h4>\
+<h4><a href="../view/en.html"> View</a></h4>';
