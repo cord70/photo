45fb9312d4779b02441506d766f6795fd4bdadc2
cord70 <emd.kornienko@gmail.com>
2025-05-23 22:00:12 +0300

    Add files via upload

---
+++ photo/animal/navAnimal.js
@@ -0,0 +1,26 @@
+var menu='\
+<h4><a href="index.html"> Животные</a></h4>\
+  <p><a href="salamandra.html"> Тритон</a></p>\
+  <p><a href="frog-pond.html"> Лягушонок у пруда</a></p>\
+  <p><a href="frog-portrait.html"> Лягушонок в пруду</a></p>\
+  <p><a href="cats-eye.html"> Глаза кошки</a></p>\
+  <p><a href="squirrel.html"> Белка на сосне</a></p>\
+  <p><a href="snail.html"> Любопытная улитка</a></p>\
+  <p><a href="snake.html"> Может кобра?</a></p>\
+  <p><a href="sand-lizard.html"> Круглоголовка</a></p>\
+  <p><a href="deer-face.html"> Портрет оленя</a></p>\
+  <p><a href="rat.html"> Крысы едят руками</a></p>\
+<h4><a href="../bird/index.html"> Птицы</a></h4>\
+<h4> Новости биологии</h4>\
+  <p><a href="eat-less.html"> Меньше ешь!</a></p>';
+
+var menuen='\
+<h4><a href="en.html"> Animals</a></h4>\
+  <p><a href="snake-en.html"> Maybe a cobra?</a></p>\
+  <p><a href="sand-lizard-en.html"> Sand lizard</a></p>\
+  <p><a href="deer-face-en.html"> Deer</a></p>\
+  <p><a href="rat-en.html"> Rat eats by hands</a></p>\
+  <p><a href="snail-en.html"> Snail in night</a></p>\
+  <p><a href="squirrel-en.html"> Squirrel</a></p>\
+  <p><a href="cats-eye-en.html"> Color of cat eyes</a></p>\
+<h4><a href="../bird/en.html"> Birds</a></h4>';
