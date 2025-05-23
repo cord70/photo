bf0fcb6a367839c16d004ae129224907e06ff8b3
cord70 <emd.kornienko@gmail.com>
2025-05-23 21:55:35 +0300

    Add files via upload

---
+++ workshop/pond/navPond.js
@@ -0,0 +1,9 @@
+var menu='\
+<h4> Дача</h4>\
+  <p><a href="../da4a/da4a.html"> Умная дача</a></p>\
+  <p><a href="../da4a/adc.html"> Считывание счётчика</a></p>\
+  <p><a href="../da4a/firewood-shed.html"> Дровница</a></p>\
+<h4> Пруд</h4>\
+  <p><a href="ornamental-pond.html"> Декоративный пруд</a></p>\
+  <p><a href="bush.html"> • Удаление куста</a></p>\
+  <p><a href="water-for-pond.html"> • Вода для пруда</a></p>';
