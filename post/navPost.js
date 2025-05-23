245cfa044150b2069facab558bc9fc000399e0de
cord70 <emd.kornienko@gmail.com>
2025-05-23 21:42:55 +0300

    Add files via upload

---
+++ post/navPost.js
@@ -0,0 +1,10 @@
+var menu='\
+<h4> сайт cord70.github.io/photo</h4>\
+  <p><a href="../workshop/site.html"> Об этом сайте</a></p>\
+  <p><a href="../search.html"> Поиск по сайту</a></p>\
+  <p><a href="email-en.html"> English page</a></p>';
+
+var menuen='\
+<h4> the site cord70.github.io/photo</h4>\
+  <p><a href="../search-en.html"> Search at this site</a></p>\
+  <p><a href="email.html"> Русская страница</a></p>';
