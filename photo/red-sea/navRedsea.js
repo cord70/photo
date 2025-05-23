1dafab2678a0f1f1d41a6a3e8018230e2da10bd0
cord70 <emd.kornienko@gmail.com>
2025-05-23 22:09:22 +0300

    Add files via upload

---
+++ photo/red-sea/navRedsea.js
@@ -0,0 +1,29 @@
+var menu='\
+<h4> На Красном море</h4>\
+  <p><a href="reef_heron.html"> Рифовая цапля</a></p>\
+  <p><a href="coral_ancient.html"> Коралловая гора</a></p>\
+  <p><a href="reef_old_ship.html"> Коралловый риф</a></p>\
+  <p><a href="arabic_numerals.html"> Арабские цифры</a></p>\
+  <p><a href="conrad_blossom_view.html"> Цветы в Конраде</a></p>\
+  <p><a href="tyran_island_sunrise.html"> Восход солнца</a></p>\
+  <p><a href="horizon.html"> Море до горизонта</a></p>\
+  <p><a href="coral-fossil.html"> Окаменевшие кораллы</a></p>\
+  <p><a href="mosque.html"> Мечеть на закате</a></p>\
+  <p><a href="rusty-ship.html"> Корабль на рифе</a></p>\
+  <p><a href="conrad_map.html"> Конрад со спутника</a></p>\
+<h4><a href="../red-sea-fish/index.html"> Рыбы Красного моря</a></h4>';
+
+var menuen='\
+<h4> At Red Sea</h4>\
+  <p><a href="horizon-en.html"> Sea horizon</a></p>\
+  <p><a href="tyran_island_sunrise-en.html"> Sunrise</a></p>\
+  <p><a href="reef_heron-en.html"> Reef heron</a></p>\
+  <p><a href="reef_old_ship-en.html"> Coral reef</a></p>\
+  <p><a href="conrad_blossom_view-en.html"> Blossom at Conrad</a></p>\
+  <p><a href="arabic_numerals-en.html"> Arabian numerals</a></p>\
+  <p><a href="mosque-en.html"> A mosque at sunset</a></p>\
+  <p><a href="coral_ancient-en.html"> Coral sand mountain</a></p>\
+  <p><a href="coral-fossil-en.html"> Corals in stone</a></p>\
+  <p><a href="rusty-ship-en.html"> A ship at reef</a></p>\
+  <p><a href="conrad_map-en.html"> Conrad Google map</a></p>\
+<h4><a href="../red-sea-fish/en.html"> Red Sea Fish</a></h4>';
